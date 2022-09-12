7a5f1ac59340b8ad66e05accc2f1af23fa395cd7
pschlo <70105856+pschlo@users.noreply.github.com>
2022-09-12 10:38:36 +0200

    added env schema file

---
+++ env-schema.sh
@@ -0,0 +1,17 @@
+# use $ROOT to refer to the directory the script is located in
+# general
+export RESTIC_REPOSITORY=
+export RESTIC_PASSWORD=
+export SOURCE_PATH=
+export DELAY=  # delay between restic calls in seconds
+export TAG=
+
+# restic forget
+export KEEP_DAILY=
+export KEEP_WEEKLY=
+export KEEP_MONTHLY=
+export KEEP_YEARLY=
+
+# Telegram
+export TOKEN=
+export CHAT_ID=
